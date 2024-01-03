f92a90375fe781b264b693cb052c768355f5aecb
thiagcarvalho <thiagoc413@gmail.com>
2024-01-03 10:20:26 -0300

    Atualizações nos arquivos

--- codigo_tex/pop_inicial.py
+++
@@ -1,7 +0,0 @@
-def pop_inicial(self):
-    pop = []
-    for i in range(0, self.popsize):
-        ind = individuo(self.df)
-        ind.gera_aleatorio()
-        ind.calc_fitness()
-        pop.append(ind)
